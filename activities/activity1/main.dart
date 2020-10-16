caef3369887c8b9be11ffdc30183e9e29a107bfd
Neil Ryan <58574397+NeilRyan420@users.noreply.github.com>
2020-10-16 23:40:50 +0800

    Sustiguer Activity 1
    Submission for Activity 1

--- activities/activity1/main.dart
+++ activities/activity1/main.dart
@@ -9,6 +9,8 @@ void main() {
 
   print("Enter First Name: ");
   String fn = stdin.readLineSync();
+  print("Enter Middle Name: ");
+  String mn = stdin.readLineSync();
   print("Enter Last Name: ");
   String ln = stdin.readLineSync();
   print("Enter Age: ");
@@ -16,25 +18,33 @@ void main() {
   int age = int.parse(strAge);
   Person p3 = Person(fn,ln,age);
   print(p3.getFullName());
+  Student p = Student(fn, mn, ln, age);
+  print(p.getFullName());
 }
 
 class Person {
   static String company = 'ABC Company';
   String firstName, lastName;
+class Student {
+  static String department = 'Computer Science';
+  String firstName, lastName, middleName;
   int age = 18;
   String _dept;
 
   String get dept => _dept;
 
   Person(this.firstName, this.lastName, this.age);
+  Student(this.firstName, this.middleName, this.lastName, this.age);
 
   //named constructor
   Person.withLNandAgeOnly(this.lastName, this.age);
+  Student.withLNandAgeOnly(this.lastName, this.age);
 
   //Person p = Person();
   //p.getFullName();
   String getFullName() {
     return ("$firstName $lastName age is $age");
+    return ("Hello $firstName $middleName $lastName, $age, from $department department");
   }
 
   //Person.getInfo()
@@ -45,10 +55,9 @@ class Person {
   //
   @override
   String toString() {
-    return lastName;
-  }
-
+@@ -51,4 +42,4 @@ class Person {
   void setLastName(String lastName) {
     this.lastName = lastName;
   }
+} 
 }
