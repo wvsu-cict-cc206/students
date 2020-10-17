bb104384224de917523b4fc7680276aef320342f
Tina Marie F. Santisteban <72536112+trinaamariee@users.noreply.github.com>
2020-10-17 21:26:35 +0800

    Santisteban Activity #1
    Activity 1

--- activities/activity1/main.dart
+++ activities/activity1/main.dart
@@ -1,5 +1,4 @@
-import 'dart:io';
-
+mport 'dart:io';
 void main() {
   Person p1 = Person('Jane','Alba', 25);
   print(p1.getFullName());
@@ -8,47 +7,60 @@ void main() {
   print(p2.getFullName());
 
   print("Enter First Name: ");
-  String fn = stdin.readLineSync();
+  String first = stdin.readLineSync();
+  print("Enter Middle Name: ");
+  String middle = stdin.readLineSync();
   print("Enter Last Name: ");
-  String ln = stdin.readLineSync();
+  String last = stdin.readLineSync();
   print("Enter Age: ");
   String strAge = stdin.readLineSync();
   int age = int.parse(strAge);
-  Person p3 = Person(fn,ln,age);
+  Person p3 = Person(first,last,age);
   print(p3.getFullName());
+  Student p = Student(first, middle, last, age);
+  print(p.getFullName());
 }
 
 class Person {
   static String company = 'ABC Company';
   String firstName, lastName;
+  class Student {
+  static String department = 'Computer Science';
+  String firstName, lastName, middleName;
   int age = 18;
   String _dept;
 
   String get dept => _dept;
 
   Person(this.firstName, this.lastName, this.age);
+  Student(this.firstName, this.middleName, this.lastName, this.age);
 
   //named constructor
   Person.withLNandAgeOnly(this.lastName, this.age);
+  Student.withLNandAgeOnly(this.lastName, this.age);
 
   //Person p = Person();
   //p.getFullName();
   String getFullName() {
-    return ("$firstName $lastName age is $age");
+  return ("$firstName $lastName age is $age");
+  return ("Hello $firstName $middleName $lastName, $age, from $department department");
   }
 
   //Person.getInfo()
   static String getInfo() {
-    return 'This is a Person class';
+  return 'This is a Person class';
   }
 
   //
   @override
   String toString() {
-    return lastName;
+  return lastName;
   }
 
+  class Person {
   void setLastName(String lastName) {
-    this.lastName = lastName;
+  this.lastName = lastName;
+  }
+  }
   }
 }
